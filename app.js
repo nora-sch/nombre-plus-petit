e63e3c02c580b2220a9f67195b6369c4562c47ab
nora-sch <nora.schumann.development@gmail.com>
2023-04-20 15:13:52 +0200

    version shorter 2

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
 notes = [12, 4, 7, 18, 1, 14];
 
 function findSmallest(table) {
-  // définir une variable vide pour le résultat
-  let result;
+  // définir une variable pour le résultat qui a le valeur de premier numèro de la table
+  let result =   table[0];
   // pacourir le tableau avec valeur - clé
   table.forEach((number, key) => {
-    // mettre le premier numèro dans le resultat
-    if (key === 0) {
-      result = table[key];
-    } else {
-    // comparer le resultat avec des autres numèros
+    // comparer le resultat actuel avec des autres numèros
     // s'il est plus grand - remplacer le valeur du resultat 
     // par le numèro iteré actuellement
       if (result > number) {
         result = number;
       }
-    }
   });
   // returner le nombre plus petit
   return result;
 }
 // afficher le resultat
 console.log(findSmallest(notes));
+
+
+
+
+
